2c479b7a95a17cbba6bd198f6e62b02f24c06db6
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-01-09 11:58:30 +0100

    Create main function with user input try-except statement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 """
 Check, if the number is bigger or lesser than generated number.
 
@@ -7,11 +8,26 @@ val (int), check (int): Compared numbers
 Returns:
 str: Returning information
 """
+
+
 def check_number(val:int, check:int) -> str:
     if val > check:
         return "To big!"
     elif val < check:
         return "To small!"
     return "You win!"
+
+
+if __name__ == "__main__":
+    generated_number = random.randint(1,100)
+    while True:
+        try:
+            player_number = int(input("Guess the number between 1 to 100: "))
+        except ValueError:
+            print("It's not a number!")
+            continue
+        except KeyboardInterrupt:
+            print("Data entry was interrupted!")
+
 
 
