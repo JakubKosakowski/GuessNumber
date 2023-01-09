ca9cfcbff964f3855cf896bacd558e420475b454
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-01-09 11:40:58 +0100

    Utworzenie funkcji check_number

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Check, if the number is bigger or lesser than generated number.
+
+Parameters:
+n (int): Checked number
+
+Returns:
+bool: Returning value
+"""
+def check_number(n:int) -> bool:
+    pass
